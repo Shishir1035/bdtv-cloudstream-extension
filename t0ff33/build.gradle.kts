f65c80958b86e9d5686d618552b8f74020c62304
md.shohanuzzaman <md.shohanuzzaman@niftycoders.com>
2026-06-07 10:42:58 +0600

    feat: add t0ff33 provider

---
+++ t0ff33/build.gradle.kts
@@ -0,0 +1,23 @@
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "Live stream aggregator (M3U) — set A."
+    authors = listOf("Shishir1035")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     */
+    status = 1
+
+    // List of video source types. Live TV.
+    tvTypes = listOf("Live")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=toffeelive.com&sz=%size%"
+
+    isCrossPlatform = true
+}
